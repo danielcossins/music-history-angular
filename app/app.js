73fb39695203e5c4cac35bfbe3b62986b317ab14
danielcossins11 <daniel.cossins@yahoo.com>
2015-08-27 10:12:09 -0500

    populating with partials

--- app/app.js
+++ app/app.js
@@ -1,1 +1,17 @@
-var app = angular.module("MusicApp", []);
+var app = angular.module("MusicApp", ['ngRoute']);
+
+app.config(['$routeProvider',
+  function($routeProvider) {
+  $routeProvider.
+    when('/songs', {
+      templateUrl: './partials/song-list.html',
+      controller: 'SongCtrl'
+    }).
+    // when('/songs/:songId', {
+    //   templateUrl: './partials/song-detail.html',
+    //   controller: 'SongDetailCtrl'
+    // }).
+    otherwise({
+      redirectTo: '/songs'
+    });
+}]);
