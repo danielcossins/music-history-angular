4d968989d5a29cb32116f909224d730f93f7b108
danielcossins11 <daniel.cossins@yahoo.com>
2015-08-28 09:07:15 -0500

    populating from from partials

--- app/app.js
+++ app/app.js
@@ -1,4 +1,13 @@
-var app = angular.module("MusicApp", ['ngRoute']);
+var app = angular.module("MusicApp", ['ngRoute', 'firebase']);
+
+// app.controller("SampleCtrl", function($scope, $firebaseObject) {
+//   var ref = new Firebase("https://flickering-fire-4801.firebaseio.com");
+//   // download the data into a local object
+//   var syncObject = $firebaseObject(ref);
+//   // synchronize the object with a three-way data binding
+//   // click on `index.html` above to see it used in the DOM!
+//   syncObject.$bindTo($scope, "data");
+// });
 
 app.config(['$routeProvider',
   function($routeProvider) {
--- app/controllers/songs.js
+++ app/controllers/songs.js
@@ -1,6 +1,17 @@
-app.controller("SongCtrl", ["$scope", "getSongs", 
-  function($scope, getSongs) {
+app.controller("SongCtrl", ["$scope", "getSongs",
+  function($scope, getSongs/*, $firebaseObject*/) {
+  // var ref = new Firebase("https://flickering-fire-4801.firebaseio.com");
+  // // $scope.data = $firebaseObject(ref);
 
+  // // download the data into a local object
+  // var syncObject = $firebaseObject(ref);
+  // // synchronize the object with a three-way data binding
+  // // click on `index.html` above to see it used in the DOM!
+  // syncObject.$bindTo($scope, "data");
+  // console.log(syncObject);
+
+
+///////////////////firebase
   $scope.killSong = function(song) {
     var songIndex = $scope.songs.indexOf(song);
     if (songIndex >= 0) {
