9a28e4035c797d645be927fd5fec3d9db5211770
danielcossins11 <daniel.cossins@yahoo.com>
2015-08-28 09:56:39 -0500

    song-details working

--- app/controllers/each-song.js
+++ app/controllers/each-song.js
@@ -6,22 +6,25 @@ app.controller("SongDetailCtrl", ["$scope", "$routeParams", "getSongs",
     console.log($routeParams);
     console.log("$scope.songId", $scope.songId);
 
-    getSongs.then(
-      function(promiseResolutionData) {
-        console.log("promiseResolutionData", promiseResolutionData);
+    $scope.selectedSong = getSongs.filter(function(song){
+      return song.songId ===parseInt($scope.songId);
+    })[0];
+    // getSongs.then(
+    //   function(promiseResolutionData) {
+    //     console.log("promiseResolutionData", promiseResolutionData);
 
-        $scope.selectedSong = promiseResolutionData.filter(function(song) {
-          console.log(song);
-          console.log($scope.songId);
-          console.log($scope.selectedSong);
-          return song.songId === parseInt($scope.songId);
-        })[0];
+    //     $scope.selectedSong = promiseResolutionData.filter(function(song) {
+    //       console.log(song);
+    //       console.log($scope.songId);
+    //       console.log($scope.selectedSong);
+    //       return song.songId === parseInt($scope.songId);
+    //     })[0];
 
-        console.log("$scope.selectedSong", $scope.selectedSong);
-      },
-      function(promiseRejectionError) {
-        console.log("error", promiseRejectionError);
-      }
-    );
+    //     console.log("$scope.selectedSong", $scope.selectedSong);
+    //   },
+    //   function(promiseRejectionError) {
+    //     console.log("error", promiseRejectionError);
+    //   }
+    // );
   }
 ]);
