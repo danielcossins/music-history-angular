9a219de7ce4eeaa8e0b547734597dd0a9b291602
danielcossins11 <daniel.cossins@yahoo.com>
2015-09-03 11:51:47 -0500

    angular is loading

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 require.config({
   paths: {
-      'angular' : './lib/bower_components/angular/angular',
-      'ngResource': './lib/bower_components/angular-resource/angular-resource',
-      'ngCookies': './lib/bower_components/angular-cookies/angular-cookies',
-      'ngProgressLite': './lib/bower_components/ngprogress-lite/ngprogress-lite',
+      'angular' : '../../lib/bower_components/angular/angular',
+      // 'ngResource': '../../lib/bower_components/angular-resource/angular-resource',
+      // 'ngCookies': '../../lib/bower_components/angular-cookies/angular-cookies',
+      // 'ngProgressLite': '../../lib/bower_components/ngprogress-lite/ngprogress-lite',
 
 
 
 
-    'jquery': './lib/bower_components/jquery/dist/jquery.min',
-    'lodash': './lib/bower_components/lodash/lodash.min',
-    'firebase': './lib/bower_components/firebase/firebase',
-    'hbs': './lib/bower_components/require-handlebars-plugin/hbs',
-    'bootstrap': './lib/bower_components/bootstrap/dist/js/bootstrap.min',
-    'q': './lib/bower_components/q/q',
-    'matchHeight': './lib/bower_components/matchHeight/jquery.matchHeight-min',
-    'es6': './lib/bower_components/requirejs-babel/es6',
-    'babel': './lib/bower_components/requirejs-babel/babel-5.8.22.min'
+    'jquery': '../../lib/bower_components/jquery/dist/jquery.min',
+    'lodash': '../../lib/bower_components/lodash/lodash.min',
+    'firebase': '../../lib/bower_components/firebase/firebase',
+    'hbs': '../../lib/bower_components/require-handlebars-plugin/hbs',
+    'bootstrap': '../../lib/bower_components/bootstrap/dist/js/bootstrap.min',
+    'q': '../../lib/bower_components/q/q',
+    'matchHeight': '../../lib/bower_components/matchHeight/jquery.matchHeight-min',
+    'es6': '../../lib/bower_components/requirejs-babel/es6',
+    'babel': '../../lib/bower_components/requirejs-babel/babel-5.8.22.min'
   },
   shim: {
       ngResource: {
